eb62af4eefc83325294f7d3adf213b5cf4db3937
Simon Robert <srob@srob.fr>
2017-08-21 15:49:37 +0200

    Placed grammar tests in tests/grammar/

--- tests/grammar/methodTest.js
+++ tests/grammar/methodTest.js
@@ -1,7 +1,7 @@
 const _ = require("lodash");
 const assert = require('assert');
 const Parser = require("microparser").Parser;
-const g = require(__dirname + "/../index.js").grammar;
+const g = require(__dirname + "/../../index.js").grammar;
 
 const parser = new Parser();
 
